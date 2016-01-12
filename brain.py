b8e7f1321fdfd15df322a10635c5bbc321ba80cc
Constantin S. Pan <kvapen@gmail.com>
2016-01-12 23:06:03 +0300

    Update the license :)

--- brain.py
+++ brain.py
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 # We wrote this file. As long as you retain this notice you can do whatever you
 # want with this stuff. If we meet some day, and you think this stuff is worth
-# it, you can buy us a bottle of cider in return.
+# it, you can buy each us a bottle of cider in return.
 #
 #	Constantin S. Pan <kvapen@gmail.com>
 #	Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
